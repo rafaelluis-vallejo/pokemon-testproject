f56436c246fa66a37d5969cd6b0bfb8c32a814b9
Rafael Vallejo <rafaelluis.vallejo@gmail.com>
2021-02-06 20:37:31 +0800

    Finished Client CSS Form (App.css)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Pokedex / <i> Add Pokemon </i></h1>
-      <input type="text" name="pokemonName" />
-      <input type="number" name="pokemonlvl" />
-      <select name="pokemonType">
-      <option value="" disabled selected>Select your option</option>
-      </select>
-    
+
+      
+
+      <div className="container">
+        <h2>
+          Pokedex  <i> // Add Pokemon </i>
+        </h2>
+
+
+        <form>
+          <input placeholder="Enter Pokemon Name" type="text" name="pokemonName" />
+          
+          <input placeholder="Enter Pokemon Level" type="number" name="pokemonlvl" />
+          <select name="pokemonType">
+            <option value="" disabled selected>
+              Select Pokemon Type
+            </option>
+          </select>
+          <button> Save </button>
+        </form>
+
+      </div>
+
     </div>
   );
 }
